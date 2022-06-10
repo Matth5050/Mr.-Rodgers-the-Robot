ecd34e44e69c25ef168ad208f7ccb32812718ca2
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-10 09:54:43 -0700

    completed second test and associated function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,23 @@ function beepBoop(input) {
   }
   return(newArray);
 }
+
+function numToSpeech() {
+  let testArray = [0,1,2,3,4,5,6,7,8,9,10];
+  let outputArray = [];
+  testArray.forEach(function(element) {
+    let num = element.toString();
+    if (num.includes(3)) {
+      outputArray.unshift("Won't you be my neighbor?")
+    } else if (num.includes(2)) {
+      outputArray.unshift("Boop!")
+    } else if (num.includes(1)) {
+      outputArray.unshift("Beep!")
+    } else {
+      outputArray.unshift(num)
+    }
+  });
+  console.log(outputArray.reverse());
+};
+
+numToSpeech();
